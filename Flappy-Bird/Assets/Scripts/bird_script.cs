1e0df95b5fe72cca701f4f9fdb5bde2244e66c75
safakozdek <safakozdek@gmail.com>
2020-01-10 17:37:24 +0300

    Fly mechanism added

---
+++ Flappy-Bird/Assets/Scripts/bird_script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird_script : MonoBehaviour
+{
+    public float speedMultiplier = 1;
+    private Rigidbody2D birdBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        birdBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            birdBody.velocity = Vector2.up * speedMultiplier;
+        }
+    }
+}
